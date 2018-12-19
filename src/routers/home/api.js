3bfffa6db523e5a469b0f781a457cdea30e94f52
Lantic <lanticgan@outlook.com>
2018-12-19 20:51:53 +0800

    调整推荐电影api

--- src/routers/home/api.js
+++ src/routers/home/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const getInTheaters = () => {
   return api
-    .get("/")
+    .get("/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a")
     .then(function(response) {
       return response;
     })
